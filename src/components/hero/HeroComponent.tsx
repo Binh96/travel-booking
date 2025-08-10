4899c06ef295ba9e07ae9011d5f063cc76251a99
Binhpx <phanxuanbinh96@gmail.com>
2025-08-10 23:26:34 +0700

    [feat] create hero component

---
+++ src/components/hero/HeroComponent.tsx
@@ -0,0 +1,10 @@
+import './HeroComponent.css';
+
+const HeroComponent = () => {
+  return (
+    <div className="hero-container">
+    </div>
+  );
+}
+
+export default HeroComponent;
--- src/page/MainPageComponent.tsx
+++ src/page/MainPageComponent.tsx
@@ -1,11 +1,16 @@
 import HeaderComponent from '../components/header/HeaderComponent';
+import HeroComponent from '../components/hero/HeroComponent';
 import './MainPageComponent.css';
 
 const MainPageComponent = () => {
   return (
     <div className="main-container">
+      <div className="decore-wrapper">
+        <img src="/image/decore.png" alt="" />
+      </div>
       <div className="inner-wrapper">
         <HeaderComponent />
+        <HeroComponent />
       </div>
     </div>
   );
